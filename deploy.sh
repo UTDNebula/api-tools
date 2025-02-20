82dc56bbf695117a29f43b1b50cb7086d2dc96ec
Tyler Hill <gotyhill@gmail.com>
2025-02-19 23:08:56 -0600

    scrape mazevo too

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ API_TOOLS_ENV=$(gcloud secrets versions access latest --secret="$ENV_SECRET_NAME
 echo "$API_TOOLS_ENV" > .env
 
 # Scrape, parse, and upload
-#exec ./api-tools -scrape -mazevo -verbose
+exec ./api-tools -scrape -mazevo -verbose
 #exec ./api-tools -parse -mazevo -verbose
 exec ./api-tools -scrape -astra -verbose
 #exec ./api-tools -parse -astra -verbose
