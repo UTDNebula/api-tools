1c521cc786e41478a67d6a850e97f7d4637fc4ee
Tyler Hill <gotyhill@gmail.com>
2025-02-20 01:23:36 -0600

    remove exec

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ API_TOOLS_ENV=$(gcloud secrets versions access latest --secret="$ENV_SECRET_NAME
 echo "$API_TOOLS_ENV" > .env
 
 # Scrape, parse, and upload
-exec ./api-tools -scrape -mazevo -verbose
-#exec ./api-tools -parse -mazevo -verbose
-exec ./api-tools -scrape -astra -verbose
-#exec ./api-tools -parse -astra -verbose
-#exec ./api-tools -upload -events -verbose
+./api-tools -scrape -mazevo -verbose
+#./api-tools -parse -mazevo -verbose
+./api-tools -scrape -astra -verbose
+#./api-tools -parse -astra -verbose
+#./api-tools -upload -events -verbose
