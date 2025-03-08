44147e1000bb463fa1c6acb0f05cc054d6ea0112
Ian <94922205+SoggyRhino@users.noreply.github.com>
2025-03-08 13:40:10 -0600

    Update sectionParser.go (#61)
    * Update sectionParser.go
    
    remove print
    
    * Update sectionParser.go
    
    unused import

--- parser/sectionParser.go
+++ parser/sectionParser.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"encoding/json"
 	"regexp"
 	"strings"
 	"time"
@@ -50,10 +49,6 @@ func parseSection(rowInfo map[string]*goquery.Selection, classInfo map[string]st
 		Grade_distribution:    getGradeDistribution(session, sectionNumber, courseRef),
 	}
 
-	a, _ := json.Marshal(section)
-	println(string(a))
-	a, _ = json.Marshal(*courseRef)
-	println(string(a))
 	// Add new section to section map
 	Sections[section.Id] = &section
 
