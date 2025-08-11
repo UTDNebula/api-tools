91dd93ee3425fa6dfd3ddb3394a3cd096bbe4623
Tyler Hill <gotyhill@gmail.com>
2025-08-10 22:44:33 -0700

    Don't need double verbose

--- runners/daily.sh
+++ runners/daily.sh
@@ -3,8 +3,8 @@
 # for daily tasks to run
 
 # scrape, parse, and upload events
-./api-tools -headless -verbose -scrape -mazevo -verbose
-./api-tools -headless -verbose -parse -mazevo -verbose
-./api-tools -headless -verbose -scrape -astra -verbose
-./api-tools -headless -verbose -parse -astra -verbose
-./api-tools -headless -verbose -upload -events -verbose
+./api-tools -headless -verbose -scrape -mazevo
+./api-tools -headless -verbose -parse -mazevo
+./api-tools -headless -verbose -scrape -astra
+./api-tools -headless -verbose -parse -astra
+./api-tools -headless -verbose -upload -events
