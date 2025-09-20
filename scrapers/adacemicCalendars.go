59584ea29153b8e8ba74a18c66201e41e4414770
Tyler Hill <gotyhill@gmail.com>
2025-09-20 12:31:25 -0500

    Get all links to scrape

--- scrapers/adacemicCalendars.go
+++ scrapers/adacemicCalendars.go
@@ -5,6 +5,7 @@
 package scrapers
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -13,9 +14,22 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+
+	"github.com/UTDNebula/api-tools/utils"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 )
 
+type AcademicCalendar struct {
+	Title string
+	Href  string
+	Time  string
+}
+
 func ScrapeAcademicCalendars(outDir string) {
+	// Start chromedp
+	chromedpCtx, cancel := utils.InitChromeDp()
+
 	// Get sub folder from output folder
 	outSubDir := filepath.Join(outDir, "academicCalendars")
 
@@ -25,10 +39,68 @@ func ScrapeAcademicCalendars(outDir string) {
 		panic(err)
 	}
 
-	downloadPdfFromBox("https://utdallas.app.box.com/s/30v4pb6o5xjzncl5nfrgcw5wcrjz79kn", "hi", outSubDir)
+	// Go to listings page
+	chromedp.RunResponse(chromedpCtx,
+		chromedp.Navigate(`https://www.utdallas.edu/academics/calendar/`),
+	)
+
+	// Extract data from links
+	// Current
+	academicCalendars := []AcademicCalendar{AcademicCalendar{"", "", "current"}}
+	chromedp.Run(chromedpCtx, chromedp.TextContent("h2.wp-block-heading", &academicCalendars[0].Title, chromedp.ByQuery))
+	var currentNode []*cdp.Node
+	chromedp.Run(chromedpCtx, chromedp.Nodes("a.wp-block-button__link", &currentNode, chromedp.ByQuery))
+	for i := 0; i < len(currentNode[0].Attributes); i += 2 {
+		if currentNode[0].Attributes[i] == "href" {
+			academicCalendars[0].Href = currentNode[0].Attributes[i+1]
+		}
+	}
+
+	// Future list
+	var futureNodes []*cdp.Node
+	chromedp.Run(chromedpCtx,
+		chromedp.Nodes(`//h2[normalize-space(text())="Future Terms"]/following-sibling::ul[1]//a`, &futureNodes, chromedp.BySearch),
+	)
+	academicCalendars = append(academicCalendars, extractTextAndHref(futureNodes, "future", chromedpCtx)...)
+
+	// Past list
+	var pastNodes []*cdp.Node
+	chromedp.Run(chromedpCtx,
+		chromedp.Nodes(`//h2[normalize-space(text())="Past Terms"]/following-sibling::div[1]//a`, &pastNodes, chromedp.BySearch),
+	)
+	academicCalendars = append(academicCalendars, extractTextAndHref(pastNodes, "past", chromedpCtx)...)
+
+	// Don't need ChromeDP anymore
+	cancel()
+
+	// Download all PDFs
+	for _, academicCalendar := range academicCalendars {
+		downloadPdfFromBox(academicCalendar.Href, academicCalendar.Time+"-"+academicCalendar.Title, outSubDir)
+	}
 }
 
-func downloadPdfFromBox(link string, filename string, outDir string) {
+func extractTextAndHref(nodes []*cdp.Node, time string, chromedpCtx context.Context) []AcademicCalendar {
+	output := []AcademicCalendar{}
+
+	// Extract href and text
+	for _, n := range nodes {
+		var href, text string
+		// Get href attribute
+		for i := 0; i < len(n.Attributes); i += 2 {
+			if n.Attributes[i] == "href" {
+				href = n.Attributes[i+1]
+			}
+		}
+		// Get inner text
+		chromedp.Run(chromedpCtx, chromedp.TextContent(fmt.Sprintf(`a[href="%s"]`, href), &text, chromedp.ByQuery))
+
+		output = append(output, AcademicCalendar{text, href, time})
+	}
+
+	return output
+}
+
+func downloadPdfFromBox(href string, filename string, outDir string) {
 	// Create blank file
 	out, err := os.Create(filepath.Join(outDir, fmt.Sprintf("%s.pdf", filename)))
 	if err != nil {
@@ -37,7 +109,7 @@ func downloadPdfFromBox(link string, filename string, outDir string) {
 	defer out.Close()
 
 	// Pull ID from link
-	parsedLink, err := url.Parse(link)
+	parsedLink, err := url.Parse(href)
 	if err != nil {
 		panic(err)
 	}
