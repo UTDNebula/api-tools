6bf4c6fc850b17e9e419a43ffe28e28e519c85c8
Tyler Hill <gotyhill@gmail.com>
2025-09-19 23:48:05 -0500

    Scrape single calendar PDF

--- main.go
+++ main.go
@@ -45,6 +45,8 @@ func main() {
 	mazevo := flag.Bool("mazevo", false, "Alongside -scrape or -parse, signifies that Mazevo should be scraped/parsed.")
 	// Flag for map scraping, parsing, and uploading
 	mapFlag := flag.Bool("map", false, "Alongside -scrape, -parse, or -upload, signifies that the UTD map should be scraped/parsed/uploaded.")
+	// Flag for academic calendar scraping
+	academicCalendar := flag.Bool("academicCalendar", false, "Alongside -scrape, signifies that the academic calendars should be scraped.")
 
 	// Flags for parsing
 	parse := flag.Bool("parse", false, "Puts the tool into parsing mode.")
@@ -113,6 +115,8 @@ func main() {
 			scrapers.ScrapeMazevo(*outDir)
 		case *mapFlag:
 			scrapers.ScrapeMapLocations(*outDir)
+		case *academicCalendar:
+			scrapers.ScrapeAcademicCalendars(*outDir)
 		default:
 			log.Panic("You must specify which type of scraping you would like to perform with one of the scraping flags!")
 		}
