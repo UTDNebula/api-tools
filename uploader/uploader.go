516f746e6c9bb4a033f2b0fa1e0fcac8ccb421a5
Flavore669 <dirkosh2@gmail.com>
2025-10-15 00:13:54 -0500

    First commit: starting on testing upload function in uploader.go

--- uploader/uploader.go
+++ uploader/uploader.go
@@ -31,9 +31,14 @@ import (
 
 var filesToUpload [3]string = [3]string{"courses.json", "professors.json", "sections.json"}
 
+// Wrapped for testability - can be replaced with mock in unit tests
+var connectDBFunc = func() *mongo.Client {
+	return connectDB()
+}
+
 func Upload(inDir string, replace bool, staticOnly bool) {
 	//Connect to mongo
-	client := connectDB()
+	client := connectDBFunc()
 
 	// Get 5 minute context
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
