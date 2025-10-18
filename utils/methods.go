6714bea8cbcf2f20e85a8df01b2f379a653ebd45
Justin <justinschwerdtfeger245@gmail.com>
2025-10-18 15:50:10 -0500

    Fixes errors starting with capital letter

--- utils/methods.go
+++ utils/methods.go
@@ -226,12 +226,12 @@ func WriteJSON(filepath string, data interface{}) error {
 	encoder.SetIndent("", "\t")
 	err = encoder.Encode(data)
 	if err != nil {
-		return fmt.Errorf("Failed to encode JSON for %v: %v", filepath, err)
+		return fmt.Errorf("failed to encode JSON for %v: %v", filepath, err)
 	}
 
 	err = fptr.Close()
 	if err != nil {
-		return fmt.Errorf("Failed to close JSON file %v: %v", filepath, err)
+		return fmt.Errorf("failed to close JSON file %v: %v", filepath, err)
 	}
 	return nil
 }
