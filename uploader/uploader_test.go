c4908c1e355bda3ba3bbb05c217c9f07a14b98ab
Flavore669 <dirkosh2@gmail.com>
2025-11-06 00:14:06 -0600

    Finished

--- uploader/uploader_test.go
+++ uploader/uploader_test.go
@@ -1,6 +1,7 @@
 package uploader
 
 import (
+	"path/filepath"
 	"testing"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -24,14 +25,62 @@ func TestUpload(t *testing.T) {
 		staticOnly bool
 	}{
 		{
-			name:       "static only mode",
-			inDir:      "./testdata",
+			name:       "Case Basic: static only mode",
+			inDir:      filepath.Join(".", "testdata", "case_basic"),
 			replace:    false,
 			staticOnly: true,
 		},
 		{
-			name:       "full upload with replace",
-			inDir:      "./testdata",
+			name:       "Case Basic: full upload with replace",
+			inDir:      filepath.Join(".", "testdata", "case_basic"),
+			replace:    true,
+			staticOnly: false,
+		},
+		{
+			name:       "Case Edge: static only mode",
+			inDir:      filepath.Join(".", "testdata", "case_edge"),
+			replace:    false,
+			staticOnly: true,
+		},
+		{
+			name:       "Case Edge: full upload with replace",
+			inDir:      filepath.Join(".", "testdata", "case_edge"),
+			replace:    true,
+			staticOnly: false,
+		},
+		{
+			name:       "Case Multiple: static only mode",
+			inDir:      filepath.Join(".", "testdata", "case_multiple"),
+			replace:    false,
+			staticOnly: true,
+		},
+		{
+			name:       "Case Multiple: full upload with replace",
+			inDir:      filepath.Join(".", "testdata", "case_multiple"),
+			replace:    true,
+			staticOnly: false,
+		},
+		{
+			name:       "Case Relationship: static only mode",
+			inDir:      filepath.Join(".", "testdata", "case_relationship"),
+			replace:    false,
+			staticOnly: true,
+		},
+		{
+			name:       "Case Relationship: full upload with replace",
+			inDir:      filepath.Join(".", "testdata", "case_relationship"),
+			replace:    true,
+			staticOnly: false,
+		},
+		{
+			name:       "Case Sorting: static only mode",
+			inDir:      filepath.Join(".", "testdata", "case_sorting"),
+			replace:    false,
+			staticOnly: true,
+		},
+		{
+			name:       "Case Sorting: full upload with replace",
+			inDir:      filepath.Join(".", "testdata", "case_sorting"),
 			replace:    true,
 			staticOnly: false,
 		},
@@ -41,7 +90,7 @@ func TestUpload(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			// This will panic when it tries to use the nil client, but that's fine for now
 			// The goal is to test that the function calls what it should call
-			// TODO: Create different test cases
+
 			defer func() {
 				if r := recover(); r != nil {
 					t.Logf("Expected panic when database operations are attempted: %v", r)
