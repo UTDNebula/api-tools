725bee86a11f55b0ccecb3ea8cf2a203909ba310
Justin <justinschwerdtfeger245@gmail.com>
2026-04-01 11:59:02 -0500

    Add repeat error detection. (needs to have flag)

--- main.go
+++ main.go
@@ -107,13 +107,35 @@ func main() {
 				log.Fatalf("No term specified for coursebook scraping! Use -term to specify.")
 			}
 			// TODO Handle Errors, give flag to retry on panic, and a retry count for non panics, how long to wait for retry
-			err := scrapers.ScrapeCoursebook(*term, *startPrefix, *outDir, *resume)
-			if errors.Is(err, &scrapers.SetupError{}) {
-				log.Fatalf("Coursebook Scraping Setup Failed: %v", err)
-			} else if err != nil {
-				log.Fatalf("Coursebook Scraping Failed: %v", err)
-			}
+			var lastErr error = nil
+			repeatErrCount := 0
+			for {
+				err := scrapers.ScrapeCoursebook(*term, *startPrefix, *outDir, *resume)
 
+				if err == nil {
+					break // Scraping succeeded
+				}
+
+				var setupErr *scrapers.SetupError
+				if errors.As(err, &setupErr) {
+					log.Fatalf("Coursebook Scraping Setup Failed: %v", err)
+					// Exit immediately
+				}
+
+				if fmt.Sprintf("%v", lastErr) == fmt.Sprintf("%v", err) {
+					repeatErrCount++
+				} else {
+					repeatErrCount = 0
+				}
+
+				lastErr = err
+				log.Printf("Coursebook Scraping Failed: %v", err)
+				if repeatErrCount >= 3 { // Make max repeat count determined by flag
+					log.Fatalf("Coursebook Scraping Failed 3 times in a row with the same error, Exiting")
+				}
+
+				// TODO: we need to resume!!!
+			}
 		case *scrapeDiscounts:
 			scrapers.ScrapeDiscounts(*outDir)
 		case *cometCalendar:
