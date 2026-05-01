db1f12b08e5d2e9fa9e005e54d1243a987d24855
Tyler Hill <gotyhill@gmail.com>
2026-05-01 00:16:20 -0500

    Add to monthly scraper

--- runners/monthly.sh
+++ runners/monthly.sh
@@ -6,3 +6,8 @@
 ./api-tools -headless -verbose -scrape -map
 ./api-tools -headless -verbose -parse -map
 ./api-tools -headless -verbose -upload -map
+
+# scrape, parse, and upload budgets
+./api-tools -headless -verbose -scrape -budgets -useBackupBudgets
+./api-tools -headless -verbose -parse -budgets -useBackupBudgets
+./api-tools -headless -verbose -upload -budgets -useBackupBudgets
