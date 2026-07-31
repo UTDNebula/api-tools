73f8e1f686618e2da88e4263aa1ef98021554123
Justin Schwerdtfeger <justinschwerdtfeger245@gmail.com>
2026-07-31 16:50:55 -0500

    Rewrite Mazevo Scraper to use ChromeDP instead of directly requesting API. Mazevo API Key no longer needed

--- scrapers/mazevo.go
+++ scrapers/mazevo.go
@@ -5,85 +5,110 @@
 package scrapers
 
 import (
-	"bytes"
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/UTDNebula/api-tools/utils"
+	"github.com/chromedp/cdproto/network"
+
+	"github.com/chromedp/chromedp"
 )
 
 // ScrapeMazevo pulls Mazevo calendar events via the public API and stores the raw response.
 func ScrapeMazevo(outDir string) {
-	apikey, err := utils.GetEnv("MAZEVO_API_KEY")
-	if err != nil {
-		panic(err)
-	}
-
 	// Make output folder
-	err = os.MkdirAll(outDir, 0777)
+	err := os.MkdirAll(outDir, 0777)
 	if err != nil {
 		panic(err)
 	}
 
-	// Init http client
-	tr := &http.Transport{
-		MaxIdleConns:       10,
-		IdleConnTimeout:    30 * time.Second,
-		DisableCompression: true,
-	}
-	cli := &http.Client{Transport: tr}
+	ctx, cancel := utils.InitChromeDp()
+	defer cancel()
+	var reqID network.RequestID // ID for requests
+	var eventsStart time.Time   // Start time of events request
+	var eventsEnd time.Time     // End time of events request
 
-	// Start on previous date to make sure we have today's data, regardless of what timezone the scraper is in
-	date := time.Now()
-	startDate := date.Add(time.Hour * -24).Format(time.RFC3339)
-	endDate := date.Add(time.Hour * 24 * 365).Format(time.RFC3339)
+	isPending := false
+	isReceived := make(chan bool, 1)
 
-	// Request events
-	url := "https://east.mymazevo.com/api/PublicCalendar/GetCalendarEvents"
-	requestBodyMap := map[string]string{
-		"apiKey": apikey,
-		"end":    endDate,
-		"start":  startDate,
-	}
-	requestBodyBytes, _ := json.Marshal(requestBodyMap)
-	requestBody := bytes.NewBuffer(requestBodyBytes)
-	req, err := http.NewRequest("POST", url, requestBody)
-	if err != nil {
-		panic(err)
-	}
-	req.Header = http.Header{
-		"Content-type": {"application/json"},
-		"Accept":       {"application/json"},
-	}
-	res, err := cli.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	if res.StatusCode != 200 {
-		log.Panicf("ERROR: Status was: %s\nIf the status is 404, you've likely been IP ratelimited!", res.Status)
-	}
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-	res.Body.Close()
-	stringBody := string(body)
+	chromedp.ListenTarget(ctx, func(ev any) {
+		switch ev := ev.(type) {
+		case *network.EventRequestWillBeSent:
+			// Get GetEvents Request Start and End Times (ISO 8601)
+			if ev.Request.URL == "https://east.mymazevo.com/api/PublicCalendar/GetEvents" {
+				rawPostData := ev.Request.PostDataEntries[0].Bytes
+				decodedPostData, err := base64.StdEncoding.DecodeString(rawPostData)
+				if err != nil {
+					log.Panic(err)
+				}
+				var data map[string]any
 
-	log.Printf("Scraped Mazevo up to %s!", endDate)
+				err = json.Unmarshal(decodedPostData, &data)
+				if err != nil {
+					log.Panic(err)
+				}
+				eventsStart, err = time.Parse(time.RFC3339, data["start"].(string))
+				if err != nil {
+					log.Panic(err)
+				}
+				eventsEnd, err = time.Parse(time.RFC3339, data["end"].(string))
+				if err != nil {
+					log.Panic(err)
+				}
 
-	// Write event data to output file
-	fptr, err := os.Create(fmt.Sprintf("%s/mazevoScraped.json", outDir))
+				// Check if end is 1 month after start
+				if eventsEnd.After(eventsStart.AddDate(0, 1, -1)) {
+					isPending = true
+				}
+			}
+		case *network.EventResponseReceived:
+			// Once Response is received, record the RequestID
+			if isPending && ev.Response.URL == "https://east.mymazevo.com/api/PublicCalendar/GetEvents" {
+				reqID = ev.RequestID
+			}
+		case *network.EventLoadingFinished:
+			// Signal that response is finished loading
+			if isPending && ev.RequestID == reqID {
+				isPending = false
+				isReceived <- true
+			}
+		}
+
+	})
+	_, err = chromedp.RunResponse(ctx,
+		chromedp.Navigate("https://east.mymazevo.com/calendar/4219c6df695c03860350ea213837fe59"),
+		chromedp.Sleep(5*time.Second),
+		chromedp.Click("input#displayMonth", chromedp.NodeVisible),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// TODO: Account for error or network issue here
+			// Wait until events have been received
+			<-isReceived
+
+			bodyBytes, err := network.GetResponseBody(reqID).Do(ctx)
+			if err != nil {
+				return fmt.Errorf("failed to get body: %w", err)
+			}
+			log.Printf("Scraped Mazevo from %s to %s!", eventsStart.Format(time.DateTime), eventsEnd.Format(time.DateTime))
+
+			// Write event data to output file
+			fptr, err := os.Create(fmt.Sprintf("%s/mazevoScraped.json", outDir))
+			if err != nil {
+				panic(err)
+			}
+			_, err = fptr.Write(bodyBytes)
+			if err != nil {
+				panic(err)
+			}
+			return nil
+		}),
+	)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
-	_, err = fptr.Write([]byte(stringBody))
-	if err != nil {
-		panic(err)
-	}
-	fptr.Close()
+
 }
